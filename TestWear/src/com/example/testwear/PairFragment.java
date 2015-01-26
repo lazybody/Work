158d02e810c022112dfbde5521b22552ff5b5d0b
Hui Wang <huiwang@mobvoiadm-ThinkPad-Edge-E440.(none)>
2015-01-26 12:59:09 +0800

    pair开始配对

--- TestWear/src/com/example/testwear/PairFragment.java
+++ TestWear/src/com/example/testwear/PairFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,18 @@ public class PairFragment extends Fragment{
         // TODO Auto-generated method stub
         View view = inflater.inflate(R.layout.fragment_welcome_circular, container, false);  
         iosBtClick(view);
+        getActivity().finish();
         return view;
+    }
+    
+    public void startPair(){
+        getActivity().sendBroadcast(new Intent("com.google.android.clockwork.action.SKIP_SETUP"));
+        Settings.Global.putInt(getActivity().getContentResolver(), "device_provisioned", 1);
+        ComponentName componentName = new ComponentName("com.google.android.apps.wearable.setup","com.google.android.clockwork.setup.MainActivity");
+        getActivity().getPackageManager().setComponentEnabledSetting(componentName,2,1);
+        Intent it = new Intent("android.intent.action.MAIN");
+        it.addCategory("android.intent.category.HOME");
+        startActivity(it);
     }
     
     private void iosBtClick(View view){
@@ -38,6 +50,7 @@ public class PairFragment extends Fragment{
                         "com.mobvoi.ticwear.ios.StartActivity");
                 it.setComponent(componentName);
                 startActivity(it);
+                getActivity().finish();
             }
         });
     }
